3f62cdd5de49e1c207e29510da95b55eba179f32
IoIxD <alphaproject217@gmail.com>
2022-08-17 21:46:47 -0700

    oops

--- database.go
+++ database.go
@@ -73,7 +73,7 @@ func init() {
 		err = ExecuteDirect(v)
 		if err != nil {
 			// If-oh. fuck.
-			if(!strings.Contains(err.Error(),"duplicate")) {
+			if(!strings.Contains(err.Error(),"duplicate") && !strings.Contains(err.Error(),"more than one primary key")) {
 				fmt.Println(err)
 				os.Exit(1)
 			} 
